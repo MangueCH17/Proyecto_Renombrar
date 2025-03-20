077396be1ce8221bad135cc9803a5d6b97992d83
ASUS <ASUS@DESKTOP-A5K88HQ>
2025-03-19 19:19:05 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        txtPrefijo = New TextBox()
+        btnSeleccionar = New Button()
+        dlgCarpeta = New FolderBrowserDialog()
+        btnIniciar = New Button()
+        Label3 = New Label()
+        txtRutaCarpeta = New TextBox()
+        btnFicha = New Button()
+        Label4 = New Label()
+        RBEsp = New RadioButton()
+        RBEng = New RadioButton()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Control
+        Label1.Cursor = Cursors.No
+        Label1.ImageAlign = ContentAlignment.MiddleRight
+        Label1.Location = New Point(72, 67)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(162, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Selecciona una carpeta"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(147, 164)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(122, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Escriba su prefijo"
+        ' 
+        ' txtPrefijo
+        ' 
+        txtPrefijo.AccessibleName = ""
+        txtPrefijo.Location = New Point(275, 161)
+        txtPrefijo.Name = "txtPrefijo"
+        txtPrefijo.Size = New Size(209, 27)
+        txtPrefijo.TabIndex = 2
+        ' 
+        ' btnSeleccionar
+        ' 
+        btnSeleccionar.AccessibleName = "btnSeleccionar"
+        btnSeleccionar.BackColor = SystemColors.Control
+        btnSeleccionar.Image = CType(resources.GetObject("btnSeleccionar.Image"), Image)
+        btnSeleccionar.Location = New Point(230, 60)
+        btnSeleccionar.Name = "btnSeleccionar"
+        btnSeleccionar.Size = New Size(39, 34)
+        btnSeleccionar.TabIndex = 3
+        btnSeleccionar.UseVisualStyleBackColor = False
+        ' 
+        ' btnIniciar
+        ' 
+        btnIniciar.BackColor = SystemColors.Control
+        btnIniciar.Image = CType(resources.GetObject("btnIniciar.Image"), Image)
+        btnIniciar.Location = New Point(173, 236)
+        btnIniciar.Name = "btnIniciar"
+        btnIniciar.Size = New Size(51, 57)
+        btnIniciar.TabIndex = 4
+        btnIniciar.UseVisualStyleBackColor = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(59, 254)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(108, 20)
+        Label3.TabIndex = 5
+        Label3.Text = "Renombrar RIS"
+        ' 
+        ' txtRutaCarpeta
+        ' 
+        txtRutaCarpeta.Location = New Point(275, 64)
+        txtRutaCarpeta.Name = "txtRutaCarpeta"
+        txtRutaCarpeta.Size = New Size(234, 27)
+        txtRutaCarpeta.TabIndex = 6
+        ' 
+        ' btnFicha
+        ' 
+        btnFicha.BackColor = SystemColors.Control
+        btnFicha.Image = CType(resources.GetObject("btnFicha.Image"), Image)
+        btnFicha.Location = New Point(522, 238)
+        btnFicha.Name = "btnFicha"
+        btnFicha.Size = New Size(51, 57)
+        btnFicha.TabIndex = 7
+        btnFicha.UseVisualStyleBackColor = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(331, 223)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(174, 20)
+        Label4.TabIndex = 8
+        Label4.Text = "Renombrar Ficha Técnica"
+        ' 
+        ' RBEsp
+        ' 
+        RBEsp.AutoSize = True
+        RBEsp.Location = New Point(377, 246)
+        RBEsp.Name = "RBEsp"
+        RBEsp.Size = New Size(82, 24)
+        RBEsp.TabIndex = 9
+        RBEsp.TabStop = True
+        RBEsp.Text = "Español"
+        RBEsp.UseVisualStyleBackColor = True
+        ' 
+        ' RBEng
+        ' 
+        RBEng.AutoSize = True
+        RBEng.Location = New Point(377, 276)
+        RBEng.Name = "RBEng"
+        RBEng.Size = New Size(69, 24)
+        RBEng.TabIndex = 10
+        RBEng.TabStop = True
+        RBEng.Text = "Inglés"
+        RBEng.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(423, 343)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(178, 40)
+        Label6.TabIndex = 12
+        Label6.Text = "Hecho por: Miguel Castro" & vbCrLf & "Practicante I+D 2024-2"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(613, 392)
+        Controls.Add(Label6)
+        Controls.Add(RBEng)
+        Controls.Add(RBEsp)
+        Controls.Add(Label4)
+        Controls.Add(btnFicha)
+        Controls.Add(txtRutaCarpeta)
+        Controls.Add(Label3)
+        Controls.Add(btnIniciar)
+        Controls.Add(btnSeleccionar)
+        Controls.Add(txtPrefijo)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.ControlText
+        Name = "Form1"
+        Text = "Renombrar RIS"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPrefijo As TextBox
+    Friend WithEvents btnSeleccionar As Button
+    Friend WithEvents dlgCarpeta As FolderBrowserDialog
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtRutaCarpeta As TextBox
+    Friend WithEvents btnFicha As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RBEsp As RadioButton
+    Friend WithEvents RBEng As RadioButton
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,208 @@
+﻿Imports System.IO
+Imports System
+Imports iTextSharp.text.pdf
+Imports iTextSharp.text.pdf.parser
+Imports System.Reflection.PortableExecutable
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+
+Public Class Form1
+    Private Sub btnSeleccionar_Click(sender As Object, e As EventArgs) Handles btnSeleccionar.Click
+        ' Mostrar el diálogo para seleccionar la carpeta
+        If dlgCarpeta.ShowDialog() = DialogResult.OK Then
+            ' Mostrar la ruta seleccionada en un TextBox (debes tener un TextBox para esto)
+            txtRutaCarpeta.Text = dlgCarpeta.SelectedPath
+        End If
+    End Sub
+
+    'INICIO CODIGO PARA LOS RIS
+
+    Private Sub btnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        ' Obtener la ruta de la carpeta 
+        Dim carpetaPDFs As String = txtRutaCarpeta.Text
+
+        ' Obtener el prefijo del TextBox
+        Dim prefijo As String = txtPrefijo.Text
+
+        ' Validar si la carpeta existe
+        If Directory.Exists(carpetaPDFs) Then
+            ' Llamar a la función para renombrar RIS
+            RenombrarRIS(carpetaPDFs, prefijo)
+            MessageBox.Show("Proceso finalizado.")
+        Else
+            MessageBox.Show("La carpeta especificada no existe.")
+        End If
+    End Sub
+
+    Public Shared Sub RenombrarRIS(carpetaPDFs As String, prefijo As String)
+        For Each nombreArchivo As String In Directory.GetFiles(carpetaPDFs, "*.pdf")
+            Dim rutaArchivo As String = IO.Path.Combine(carpetaPDFs, nombreArchivo)
+            Try
+                Dim lectorPDF As New PdfReader(rutaArchivo)
+                Dim pagina As PdfDictionary = lectorPDF.GetPageN(1) ' Asumiendo que el título está en la primera página
+
+                ' Extraer el contenido de la página como texto
+                Dim estrategiaExtraccion As New LocationTextExtractionStrategy()
+                Dim contenido As String = PdfTextExtractor.GetTextFromPage(lectorPDF, 1, estrategiaExtraccion)
+
+                ' Dividir el contenido en líneas
+                Dim lineasTexto As String() = contenido.Split({vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+
+                ' Verificar si el array tiene suficientes elementos
+                If lineasTexto.Length >= 5 Then
+                    Dim titulo As String = lineasTexto(4).Trim() ' Extraer el texto de la línea 5 (índice 4)
+
+                    ' Eliminar caracteres inválidos del título
+                    titulo = New String(titulo.Where(Function(c) Char.IsLetterOrDigit(c) OrElse c = "."c OrElse c = "_"c OrElse c = "-"c OrElse c = " "c).ToArray())
+
+                    ' Usar el prefijo al renombrar
+                    Dim nuevoNombre As String = prefijo & titulo & ".pdf"
+                    Dim nuevaRuta As String = IO.Path.Combine(carpetaPDFs, nuevoNombre)
+
+                    lectorPDF.Close()
+                    File.Move(rutaArchivo, nuevaRuta)
+
+                    Console.WriteLine("Archivo renombrado a: {0}", nuevoNombre)
+                Else
+                    Console.WriteLine("El archivo PDF {0} no tiene suficientes líneas.", nombreArchivo)
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine("Error al procesar el archivo {0}: {1}", nombreArchivo, ex.Message)
+            End Try
+        Next
+    End Sub
+
+    'INICIO DEL CODIGO PARA LAS FICHAS TECNICAS
+
+    Dim idioma As String  ' Valor predeterminado
+    Private Sub RBEsp_CheckedChanged(sender As Object, e As EventArgs) Handles RBEsp.CheckedChanged
+        If RBEsp.Checked Then
+            idioma = "Español"
+        End If
+    End Sub
+
+    Private Sub RBEng_CheckedChanged(sender As Object, e As EventArgs) Handles RBEng.CheckedChanged
+        If RBEng.Checked Then
+            idioma = "Ingles"
+        End If
+    End Sub
+
+    Private Sub btnFicha_Click(sender As Object, e As EventArgs) Handles btnFicha.Click
+        ' Obtener la ruta de la carpeta 
+        Dim carpetaPDFs = txtRutaCarpeta.Text
+
+        ' Obtener el prefijo del TextBox
+        Dim prefijo = txtPrefijo.Text
+
+        ' Validar si la carpeta existe
+        If Directory.Exists(carpetaPDFs) Then
+            ' Llamar a la función para renombrar archivos
+            If idioma = "Ingles" Then
+                ' Ejecutar la acción para documentos en inglés
+                RenombrarFTEng(carpetaPDFs, prefijo)
+                MessageBox.Show("Proceso finalizado.")
+            ElseIf idioma = "Español" Then
+                ' Ejecutar la acción para documentos en español
+                RenombrarFTEsp(carpetaPDFs, prefijo)
+                MessageBox.Show("Proceso finalizado.")
+            End If
+        Else
+            MessageBox.Show("La carpeta especificada no existe.")
+        End If
+    End Sub
+
+    Public Shared Sub RenombrarFTEsp(carpetaPDFs As String, prefijo As String)
+        For Each nombreArchivo As String In Directory.GetFiles(carpetaPDFs, "*.pdf")
+            Dim rutaArchivo As String = IO.Path.Combine(carpetaPDFs, nombreArchivo)
+
+            Try
+                Dim lectorPDF As New PdfReader(rutaArchivo)
+                Dim pagina As PdfDictionary = lectorPDF.GetPageN(1) ' Asumiendo que el título está en la primera página
+
+                ' Extraer el contenido de la página como texto
+                Dim estrategiaExtraccion As New LocationTextExtractionStrategy()
+                Dim contenido As String = PdfTextExtractor.GetTextFromPage(lectorPDF, 1, estrategiaExtraccion)
+
+                ' Dividir el contenido en líneas
+                Dim lineasTexto As String() = contenido.Split({vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+
+                ' Verificar si el array tiene suficientes elementos
+                If lineasTexto.Length >= 5 Then
+                    Dim lineaCompleta As String = lineasTexto(9).Trim() ' Obtener la línea 5 completa (índice 4)
+
+                    ' Calcular el final del título (último carácter de la línea)
+                    Dim inicio As Integer = 30 ' Índice del primer carácter que quieres extraer
+                    Dim fin As Integer = lineaCompleta.Length - 1  ' Índice del último carácter
+
+                    ' Extraer el título
+                    Dim titulo As String = lineaCompleta.Substring(inicio, fin - inicio + 1)
+
+                    ' Eliminar caracteres inválidos del título
+                    titulo = New String(titulo.Where(Function(c) Char.IsLetterOrDigit(c) OrElse c = "."c OrElse c = "_"c OrElse c = "-"c OrElse c = " "c).ToArray())
+
+                    ' Usar el prefijo al renombrar
+                    Dim nuevoNombre As String = prefijo & titulo & ".pdf"
+                    Dim nuevaRuta As String = IO.Path.Combine(carpetaPDFs, nuevoNombre)
+
+                    lectorPDF.Close()
+                    File.Move(rutaArchivo, nuevaRuta)
+
+                    Console.WriteLine("Archivo renombrado a: {0}", nuevoNombre)
+                Else
+                    Console.WriteLine("El archivo PDF {0} no tiene suficientes líneas.", nombreArchivo)
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine("Error al procesar el archivo {0}: {1}", nombreArchivo, ex.Message)
+            End Try
+        Next
+    End Sub
+
+    Public Shared Sub RenombrarFTEng(carpetaPDFs As String, prefijo As String)
+        For Each nombreArchivo As String In Directory.GetFiles(carpetaPDFs, "*.pdf")
+            Dim rutaArchivo As String = IO.Path.Combine(carpetaPDFs, nombreArchivo)
+
+            Try
+                Dim lectorPDF As New PdfReader(rutaArchivo)
+                Dim pagina As PdfDictionary = lectorPDF.GetPageN(1) ' Asumiendo que el título está en la primera página
+
+                ' Extraer el contenido de la página como texto
+                Dim estrategiaExtraccion As New LocationTextExtractionStrategy()
+                Dim contenido As String = PdfTextExtractor.GetTextFromPage(lectorPDF, 1, estrategiaExtraccion)
+
+                ' Dividir el contenido en líneas
+                Dim lineasTexto As String() = contenido.Split({vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+
+                ' Verificar si el array tiene suficientes elementos
+                If lineasTexto.Length >= 5 Then
+                    Dim lineaCompleta As String = lineasTexto(9).Trim() ' Obtener la línea 5 completa (índice 4)
+
+                    ' Calcular el final del título (último carácter de la línea)
+                    Dim inicio As Integer = 23 ' Índice del primer carácter que quieres extraer
+                    Dim fin As Integer = lineaCompleta.Length - 1  ' Índice del último carácter
+
+                    ' Extraer el título
+                    Dim titulo As String = lineaCompleta.Substring(inicio, fin - inicio + 1)
+
+                    ' Eliminar caracteres inválidos del título
+                    titulo = New String(titulo.Where(Function(c) Char.IsLetterOrDigit(c) OrElse c = "."c OrElse c = "_"c OrElse c = "-"c OrElse c = " "c).ToArray())
+
+                    ' Usar el prefijo al renombrar
+                    Dim nuevoNombre As String = prefijo & titulo & ".pdf"
+                    Dim nuevaRuta As String = IO.Path.Combine(carpetaPDFs, nuevoNombre)
+
+                    lectorPDF.Close()
+                    File.Move(rutaArchivo, nuevaRuta)
+
+                    Console.WriteLine("Archivo renombrado a: {0}", nuevoNombre)
+                Else
+                    Console.WriteLine("El archivo PDF {0} no tiene suficientes líneas.", nombreArchivo)
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine("Error al procesar el archivo {0}: {1}", nombreArchivo, ex.Message)
+            End Try
+        Next
+    End Sub
+
+End Class
